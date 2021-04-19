d3a7a91792715bb374cf683562b4eb58f37011af
MohamedAli-sys <good.heart52244@gmail.com>
2021-04-19 16:56:41 +0200

    Pages Get Ready to Start Design

--- iti-community-admin/src/routes.js
+++ iti-community-admin/src/routes.js
@@ -1,9 +1,15 @@
 import React from 'react';
 
+const Groups = React.lazy(() => import('./views/Groups/All/allGroups'))
+const addGroups = React.lazy(() => import('./views/Groups/Add/AddGroup'))
+const editGroups = React.lazy(() => import('./views/Groups/Edit/EditGroup'))
 
 
 const routes = [
   { path: '/', exact: true, name: 'Home' },
+  { path: '/groups/All', name: 'Groups', component: Groups },
+  { path: '/groups/Add', name: 'Add Group', component: addGroups },
+  { path: '/groups/:id', name: 'Edit Group', component: editGroups },
 
 ];
 
