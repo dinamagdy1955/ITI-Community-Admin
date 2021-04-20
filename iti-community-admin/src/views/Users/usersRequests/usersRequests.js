3adb964e150541682c3e773c0cf4363550f632a1
dinamagdy1955 <dinamagdy.1995@gmail.com>
2021-04-20 14:54:55 +0200

    try to solve user

--- iti-community-admin/src/views/Users/usersRequests/usersRequests.js
+++ iti-community-admin/src/views/Users/usersRequests/usersRequests.js
@@ -1,4 +1,5 @@
-import React from "react";
+import React, { useEffect } from "react";
+import db from "src/firebase";
 import {
   CBadge,
   CCard,
@@ -13,41 +14,77 @@ import {
   CLink,
 } from "@coreui/react";
 import CIcon from "@coreui/icons-react";
+
 export default function UsersRequests() {
+  var arr = [];
   const [collapsed, setCollapsed] = React.useState(true);
   const [showCard, setShowCard] = React.useState(true);
+
+  useEffect(() => {
+    const getData = async () => {
+      await db
+        .collection("users-basics")
+        .get()
+        .then((res) => {
+          res.forEach((e) => {
+            arr.push({
+              id: e.id,
+              data: e.data(),
+            });
+          });
+          console.log(arr);
+        });
+    };
+    getData();
+  }, []);
+
+  function acceptUserRequest() {}
+  function cancelUserRequest() {}
   return (
-    <>
-      <CCol xs="12" sm="6" md="4">
-        <CFade in={showCard}>
-          <CCard>
-            <CCardHeader>
-              <div className="card-header-actions">
-                <CLink className="card-header-action">
-                  <CIcon name="cil-settings" />
-                </CLink>
-                <CLink
-                  className="card-header-action"
-                  onClick={() => setCollapsed(!collapsed)}
-                >
-                  <CIcon
-                    name={collapsed ? "cil-chevron-bottom" : "cil-chevron-top"}
-                  />
-                </CLink>
-                <CLink
-                  className="card-header-action"
-                  onClick={() => setShowCard(false)}
-                >
-                  <CIcon name="cil-x-circle" />
-                </CLink>
-              </div>
-            </CCardHeader>
-            <CCollapse show={collapsed}>
-              <CCardBody></CCardBody>
-            </CCollapse>
-          </CCard>
-        </CFade>
-      </CCol>
-    </>
+    <div>
+      {arr.map((user, index) => {
+        return (
+          <CCol xs="12" sm="6" md="4">
+            <CFade in={showCard}>
+              <CCard>
+                <CCardHeader>
+                  {user}
+                  <div className="card-header-actions">
+                    <CLink
+                      className="card-header-action"
+                      onClick={() => acceptUserRequest()}
+                    >
+                      <CIcon name="cil-settings" />
+                    </CLink>
+                    <CLink
+                      className="card-header-action"
+                      onClick={() => setCollapsed(!collapsed)}
+                    >
+                      <CIcon
+                        name={
+                          collapsed ? "cil-chevron-bottom" : "cil-chevron-top"
+                        }
+                      />
+                    </CLink>
+                    <CLink
+                      className="card-header-action"
+                      onClick={() => {
+                        setShowCard(false);
+                        cancelUserRequest();
+                      }}
+                    >
+                      <CIcon name="cil-x-circle" />
+                    </CLink>
+                  </div>
+                </CCardHeader>
+                <CCollapse show={collapsed}>
+                  <CCardBody></CCardBody>
+                </CCollapse>
+              </CCard>
+            </CFade>
+          </CCol>
+        );
+      })}
+    </div>
   );
 }
