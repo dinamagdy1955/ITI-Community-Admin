c36bf4da650b889cb2dd15d2ad491fe898459525
dinamagdy1955 <dinamagdy.1995@gmail.com>
2021-04-21 05:50:28 +0200

    solve warns

--- iti-community-admin/src/Services/GroupServices.js
+++ iti-community-admin/src/Services/GroupServices.js
@@ -1,5 +1,5 @@
 // import React, { useEffect, useState } from "react";
-// import db from "src/firebase";
+import db from "src/firebase";
 // import AllGroups from "../views/Groups/All/allGroups";
 
 export function getGroupData() {
@@ -16,17 +16,13 @@ export function getGroupData() {
   //     });
   //     return arr;
   //   });
-  return db
-    .collection("Groups")
-
+  return db.collection("Groups");
 }
 
 export function addNewGroup(data) {
-  return db
-    .collection("Groups").add(data)
+  return db.collection("Groups").add(data);
 }
 
 export function delGroup(id) {
   return db.collection("Groups").doc(id).delete();
 }
-
