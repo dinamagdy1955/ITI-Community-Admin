28e0072139c21ff86daf77455253f638f12e4d18
dinamagdy1955 <dinamagdy.1995@gmail.com>
2021-04-21 03:00:27 +0200

    try to solve user data

--- iti-community-admin/src/views/Users/usersRequests/usersRequests.js
+++ iti-community-admin/src/views/Users/usersRequests/usersRequests.js
@@ -1,81 +1,73 @@
 import React, { useEffect } from "react";
+import db from "src/firebase";
 import {
   CBadge,
   CCard,
   CCardBody,
-  CCardFooter,
   CCardHeader,
   CCol,
   CRow,
-  CCollapse,
-  CFade,
-  CSwitch,
-  CLink,
+  CDataTable,
+  CButton,
 } from "@coreui/react";
 import CIcon from "@coreui/icons-react";
 import { getUnAcceptedUsers } from "src/Services/users";
 
 export default function UsersRequests() {
   var arr = [];
-
-  const [collapsed, setCollapsed] = React.useState(true);
-  const [showCard, setShowCard] = React.useState(true);
+  var l = [];
+  const [users, setUsers] = React.useState([]);
   useEffect(() => {
-    const data = getUnAcceptedUsers().then((res) => {
-      arr = res;
-      console.log(arr);
+    db.collection("users-basics").onSnapshot((res) => {
+      res.forEach((e) => {
+        if (e.data().isAccepted == false) {
+          db.collection("users-details")
+            .doc(e.id)
+            .onSnapshot((response) => {
+              //   console.log(e.id);
+              //   console.log(e.data());
+              //   console.log(response.data());
+
+              let d = {
+                id: e.id,
+                name:
+                  response.data().firstName + " " + response.data().lastName,
+                nationalID: response.data().nationalID,
+                track: response.data().track,
+                branch: response.data().branch,
+                email: e.data().email,
+              };
+              arr.push(d);
+              console.log(arr);
+              l = [...arr];
+              setUsers(l);
+              console.log(users);
+            });
+        }
+      });
     });
-  }, [arr]);
+    // setUsers(arr);
+  }, []);
   function acceptUserRequest() {}
   function cancelUserRequest() {}
+  const fields = ["name", "nationalID", "track", "branch", "email"];
   return (
-    <div>
-      {arr.map((user, index) => {
-        return (
-          <div>
-            <p>{user}</p>
-          </div>
-          // <CCol xs="12" sm="6" md="4">
-          //   <CFade in={showCard}>
-          //     <CCard>
-          //       <CCardHeader>
-          //         {user.detailedData.firstName}
-          //         <div className="card-header-actions">
-          //           <CLink
-          //             className="card-header-action"
-          //             onClick={() => acceptUserRequest()}
-          //           >
-          //             <CIcon name="cil-settings" />
-          //           </CLink>
-          //           <CLink
-          //             className="card-header-action"
-          //             onClick={() => setCollapsed(!collapsed)}
-          //           >
-          //             <CIcon
-          //               name={
-          //                 collapsed ? "cil-chevron-bottom" : "cil-chevron-top"
-          //               }
-          //             />
-          //           </CLink>
-          //           <CLink
-          //             className="card-header-action"
-          //             onClick={() => {
-          //               setShowCard(false);
-          //               cancelUserRequest();
-          //             }}
-          //           >
-          //             <CIcon name="cil-x-circle" />
-          //           </CLink>
-          //         </div>
-          //       </CCardHeader>
-          //       <CCollapse show={collapsed}>
-          //         <CCardBody></CCardBody>
-          //       </CCollapse>
-          //     </CCard>
-          //   </CFade>
-          // </CCol>
-        );
-      })}
-    </div>
+    <>
+      <CRow>
+        <CCol xs="12" lg="12">
+          <CCard>
+            <CCardHeader>Simple Table</CCardHeader>
+            <CCardBody>
+              <CDataTable
+                items={users}
+                fields={fields}
+                itemsPerPage={5}
+                pagination
+              />
+            </CCardBody>
+          </CCard>
+        </CCol>
+      </CRow>
+    </>
   );
 }
